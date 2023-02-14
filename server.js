404d409b029bb28878d4eaf28524572b908a2148
JacksenSE <Jacskenbmarshall@gmail.com>
2023-02-13 20:38:40 -0800

    sas

--- server.js
+++ server.js
@@ -1,40 +1,12 @@
 const express = require('express');
 const app = express();
-const { Sequelize } = require('sequelize');
-const morgan = require("morgan")
-require('dotenv').config()
 
-
-//MIDDLEWARE
-app.use(express.json())
-
-
-
-//get all plants
-app.get("/api/v1/plants", (req,res) => {
-    console.log("route ran")
-    res.status(200).json({
-        status:"Sucess",
-        data:{
-        plant:["Alo Vera","Spider Plant"]
-        },
-    });
+app.get('/mycoolapp/', function(req, res){
+    res.send("Hello from the root application URL");
 });
 
-//get a plant
-app.get("/api/v1/plants/:id",(req, res) => {
-    console.log(req.params);
-});
-
-//Create a plant
-app.post("/api/v1/plants",(req, res) => {
-    console.log(req.body);
-});
-
-//Update plants
-app.put("/api/v1/plants/:id",(req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
+app.get('/mycoolapp/test/', function(req, res){
+    res.send("Hello from the 'test' URL");
 });
 
 
